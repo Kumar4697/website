6d4ead55ed8fcebbee00240fbb76456706a31f19
Pragati Garg <pragati.g@consultadd.in>
2019-12-12 09:05:47 -0800

    reactproject

---
+++ src/Fcomponent/Greet.js
@@ -0,0 +1,9 @@
+import React, {component, Component} from 'react';
+
+class Greet extends Component{
+    render(){
+        return <h1>hey asg</h1>
+    }
+
+}
+export default Greet;
---
+++ src/Fcomponent/jsonComp.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class jsonComp extends Component{
+    render(){
+        return(
+            <div>
+                 <h1>hey there</h1>
+            </div>
+        );
+    }
+}
+export default jsonComp
